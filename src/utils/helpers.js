d331e7119b6e38bf2b3fa7e53b7def10848f8215
Dongjun Hwangbo <dj0419@kiotcom.co.kr>
2026-02-24 10:05:07 +0900

    #3. helpers.js 매직넘버 상수화 및 환경설정 정리

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,10 @@
+const OPTION_PRICES = {
+  extraCapacity: 100000,
+  multitrack: 100000,
+  personalMonitor: 100000,
+  extraOperator: 20000,  // per hour
+};
+
 export function parseMinutes(timeStr) {
   const [h, m] = timeStr.split(':').map(Number);
   return h * 60 + m;
@@ -5,9 +12,9 @@ export function parseMinutes(timeStr) {
 
 export function calculatePrice(options) {
   let total = 0;
-  if (options.extraCapacity) total += 100000;
-  if (options.multitrack) total += 100000;
-  if (options.personalMonitor) total += 100000;
-  if (options.extraOperator) total += 20000 * (options.extraOperatorHours || 0);
+  if (options.extraCapacity) total += OPTION_PRICES.extraCapacity;
+  if (options.multitrack) total += OPTION_PRICES.multitrack;
+  if (options.personalMonitor) total += OPTION_PRICES.personalMonitor;
+  if (options.extraOperator) total += OPTION_PRICES.extraOperator * (options.extraOperatorHours || 0);
   return total;
 }
